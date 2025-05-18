318f5e21b5711a1bf1f1121735c1d672559a6ff7
Ajeet Kumar <ajeetkumarsci460@gmail.com>
2025-05-18 19:10:22 +0530

    17

--- sol2.cpp
+++ sol2.cpp
@@ -14,11 +14,6 @@ void printif(bool a,T tp="Yes" ){if(a==true)cout<<tp<<endl;else cout<<"No"<<endl
 ll mul(ll a,ll b){a%=mod;ll res=0;while(b>0){if(b&1)res=(res+a)%mod;a=(a+a)%mod;b>>=1;}return res;}
 ll exp(ll a,ll b){a%=mod;ll res=1;while (b>0){if(b&1)res=res*a%mod;a=a*a%mod;b>>=1;}return res;}
 
-bool poss(vector<int>& v1, vector<int>& v2){
-    for(int i=0;i<v1.size();i++) if(v1[i]<v2[i]) return false;
-    return true;
-}
-
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
